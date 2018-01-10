b934cfe099f593be1b862a76da2ad8dcd8f575af
H1tomaru <watashiwa@inbox.ru>
2018-01-10 16:28:07 +0300

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -1,3 +1,14 @@
 # name: MrBug-TroikiPoisk
 # version: 9.9.9
 # authors: MrBug
+
+after_initialize do
+
+  Discourse::Application.routes.append do
+    get "MrBug" => "MrBug#index"
+    get "zaips" => "zaips#index"
+    post "oops" => "oops#add"
+    post "troikopoisk" => "troikopoisk#find"
+  end
+
+end
