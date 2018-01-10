5faf5d3c88aebe76d24f20a3037495296ac87654
H1tomaru <watashiwa@inbox.ru>
2018-01-10 17:11:04 +0300

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -2,13 +2,9 @@
 # version: 9.9.9
 # authors: MrBug
 
-after_initialize do
-
-  Discourse::Application.routes.append do
-    get "mrbug" => "mrbug#index"
-    get "zaips" => "zaips#index"
-    post "oops" => "oops#add"
-    post "troikopoisk" => "troikopoisk#find"
-  end
-
+Discourse::Application.routes.append do
+  get "/mrbug" => "mrbug#index"
+  get "/zaips" => "zaips#index"
+  post "/oops" => "oops#add"
+  post "/troikopoisk" => "troikopoisk#find"
 end
