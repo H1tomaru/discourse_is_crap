e6ebec0c9fd5a8f306907aa82a2673183e5cfb67
H1tomaru <watashiwa@inbox.ru>
2018-01-10 18:02:20 +0300

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -2,11 +2,14 @@
 # version: 9.9.9
 # authors: MrBug
 
-render json: {title:"custom route"}
+register_asset 'javascripts/discourse/templates/mrbug.hbs'
 
-#Discourse::Application.routes.append do
-#  get "/mrbug" => "mrbug#index"
-#  get "/zaips" => "zaips#index"
-#  post "/oops" => "oops#add"
-#  post "/troikopoisk" => "troikopoisk#find"
-#end
+after_initialize do
+  Discourse::Application.routes.append do
+    get "/mrbug" => "mrbug#show"
+    get "/zaips" => "zaips#show"
+    post "/oops" => "oops#add"
+    post "/troikopoisk" => "troikopoisk#find"
+#   get 'users/:username/route' => 'users#show', constraints: {username: USERNAME_ROUTE_FORMAT}
+  end
+end
