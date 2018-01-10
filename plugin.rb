e01a93e3a372097ae5ac639f5797155554f48adb
H1tomaru <watashiwa@inbox.ru>
2018-01-10 17:57:31 +0300

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -2,9 +2,11 @@
 # version: 9.9.9
 # authors: MrBug
 
-Discourse::Application.routes.append do
-  get "/mrbug" => "mrbug#index"
-  get "/zaips" => "zaips#index"
-  post "/oops" => "oops#add"
-  post "/troikopoisk" => "troikopoisk#find"
-end
+render json: {title:"custom route"}
+
+#Discourse::Application.routes.append do
+#  get "/mrbug" => "mrbug#index"
+#  get "/zaips" => "zaips#index"
+#  post "/oops" => "oops#add"
+#  post "/troikopoisk" => "troikopoisk#find"
+#end
