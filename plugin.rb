9f64fc478f1d5c879ca9c1ecec2fa32e96c337e1
H1tomaru <watashiwa@inbox.ru>
2018-01-17 22:55:03 +0300

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -7,32 +7,30 @@ after_initialize do
   require_dependency "application_controller"
 
   Discourse::Application.routes.append do
-    get '/home' => 'custom#index'
-    get '/home/page' => 'try#index'
-    get '/mrbug' => 'mrbug#index'
+    get '/home' => 'mrbug#test1'
+    get '/home/page' => 'mrbug#test2'
+    get '/MrBug' => 'mrbug#show'
   end
 
-   class ::CustomController < ActionController::Base
-  	include CurrentUser
-    def index
-   		if (current_user)
-   			redirect_to('/')
-			else
-				redirect_to('/home/page')
-			end
-    end
-  end
-
-  class ::TryController < ::ApplicationController
-    def index
-      render nothing:true
-    end
-  end
-	
   class ::MrBugController < ActionController::Base
-    def index
+  	include CurrentUser
+
+    def test1
+	if (current_user)
+		redirect_to('/')
+	else
+	redirect_to('/home/page')
+	end
+    end
+
+    def test2
       render nothing:true
     end
+	  
+    def show
+      render nothing:true
+    end 
+
   end
   
 end
