e1cd6aaae83ae48c8dd7536bc144c406accb7c29
H1tomaru <watashiwa@inbox.ru>
2018-01-17 20:29:27 +0300

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -2,20 +2,6 @@
 # version: 9.9.1
 # authors: MrBug
 
-#after_initialize do
-#  Discourse::Application.routes.append do
-#    get "/mrbug" => "mrbug#show"
-#    get "/zaips" => "zaips#show"
-#    post "/oops" => "oops#add"
-#    post "/troikopoisk" => "troikopoisk#find"
-#    get 'users/:username/route' => 'users#show', constraints: {username: USERNAME_ROUTE_FORMAT}
-#  end
-#end
-
-#Discourse::Application.routes.append do
-#  get '/admin/plugins/mrbug' => 'admin/plugins#index'
-#end
-
 after_initialize do
 
   require_dependency "application_controller"
@@ -24,29 +10,6 @@ after_initialize do
     get '/home' => 'custom#index'
     get '/home/page' => 'try#index'
     get '/mrbug' => 'mrbug#index'
-  end
-
-  class ::CustomController < ActionController::Base
-  	include CurrentUser
-    def index
-   		if (current_user)
-   			redirect_to('/')
-			else
-				redirect_to('/home/page')
-			end
-    end
-  end
-
-  class ::TryController < ::ApplicationController
-    def index
-      render nothing:true
-    end
-  end
-	
-  class ::MrBugController < ::ApplicationController
-    def index
-      render nothing:true
-    end
   end
 
 end
