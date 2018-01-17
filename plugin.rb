5ac6bd8b5811ae08aa0b5978b497cb0389d4e4ee
H1tomaru <watashiwa@inbox.ru>
2018-01-17 22:14:22 +0300

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -12,4 +12,27 @@ after_initialize do
     get '/mrbug' => 'mrbug#index'
   end
 
+   class ::CustomController < ActionController::Base
+  	include CurrentUser
+    def index
+   		if (current_user)
+   			redirect_to('/')
+			else
+				redirect_to('/home/page')
+			end
+    end
+  end
+
+  class ::TryController < ::ApplicationController
+    def index
+      render nothing:true
+    end
+  end
+	
+  class ::MrBugController < ActionController::Base
+    def index
+      render nothing:true
+    end
+  end
+  
 end
