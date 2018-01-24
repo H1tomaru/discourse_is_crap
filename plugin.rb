8d406df381aec7024e4a927b9d4ad1d7a96581bc
H1tomaru <watashiwa@inbox.ru>
2018-01-25 02:16:20 +0300

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -2,9 +2,13 @@
 # version: 9.9.1
 # authors: MrBug
 
-after_initialize do
+require 'mongo'
 
-#	require_dependency "application_controller"
+after_initialize do
+	
+	gamedb = Mongo::Connection.new("localhost", 33775).db("AutoZ_gameDB")
+	auth = gamedb.authenticate("troiko_user", "47TTGLRLR3")
+	gamedb.collection_names.each { |name| puts name }
 
 	Discourse::Application.routes.append do
 		get '/MrBug' => 'mrbug#show'
