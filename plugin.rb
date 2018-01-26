4eb40aa4e1403ca7dd785eb13036eaf92bf76458
H1tomaru <watashiwa@inbox.ru>
2018-01-26 13:43:52 +0300

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -2,29 +2,25 @@
 # version: 9.9.9
 # authors: MrBug
 
-#gems...
 gem 'bson', "4.3.0"
 gem 'mongo', "2.5.0"
 
-#require_dependency "application_controller" //wtf is this crap?
+#require_dependency "application_controller"
 
 require 'mongo'
 
-#connect to database
 gamedb = Mongo::Client.new('mongodb://troiko_user:47TTGLRLR3@91.134.133.218:33775/AutoZ_gameDB?authSource=admin')
 #	puts gamedb.collections
 #	test = gamedb.collections
 
-#some retarded crap where route is... binded... to some function... with #subfuction... 
 Discourse::Application.routes.append do
 	get '/MrBug' => 'mrbug#show'
 end
 
-#actual program...
 class ::MrbugController < ::ApplicationController
 
-#	include CurrentUser #dont even know what it is...
-	#really really actual program...
+#	include CurrentUser
+
 	def show
 		render json: { name: "donut", description: "delicious!" }
 	end 
