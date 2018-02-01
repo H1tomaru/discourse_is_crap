28a0b8a17cff6832df0764f5dd66bdc091bcc043
H1tomaru <watashiwa@inbox.ru>
2018-02-01 19:02:24 +0300

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -14,23 +14,24 @@ after_initialize do
 	Discourse::Application.routes.append do
 		get '/MrBug' => 'mrbug#show'
 	end
+	
+	db = Mongo::Client.new([ '93.171.216.230:33775' ], user: 'troiko_user', password: '47TTGLRLR3' )
+	gamedb = db.use('AutoZ_gameDB')
+	userlistdb = db.use('AutoZ_gameZ')
+	userdb = db.use('userdb')
+	userfb = db.use('userfb')
 
 	class ::MrbugController < ::ApplicationController
 
 	#	include CurrentUser
-		db = Mongo::Client.new([ '93.171.216.230:33775' ], user: 'troiko_user', password: '47TTGLRLR3' )
-		gamedb = db.use('AutoZ_gameDB')
-		userlistdb = db.use('AutoZ_gameZ')
-		userdb = db.use('userdb')
-		userfb = db.use('userfb')
-		gamelist = gamedb[:gameDB].find().limit( 10 )
-		userlist = userlistdb[:ulistP4].find().limit( 10 )
-		zapislist = userdb[:PS4db].find().limit( 10 )
-		feedbacks = userfb[:userfb].find().limit( 10 )
 
 		def show
-
-			render json: { name: "donut", description: "delicious!", gamelist: gamelist, userlist: userlist, zapislist: zapislist, feedbacks: feedbacks }
+#			gamelist = gamedb[:gameDB].find().limit( 10 )
+#			userlist = userlistdb[:ulistP4].find().limit( 10 )
+#			zapislist = userdb[:PS4db].find().limit( 10 )
+#			feedbacks = userfb[:userfb].find().limit( 10 )
+#			render json: { name: "donut", description: "delicious!", gamelist: gamelist, userlist: userlist, zapislist: zapislist, feedbacks: feedbacks }
+			@testthiscrap = "Bitch stupid!"
 		end 
 
 	end
