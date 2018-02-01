f4b1fab763941c59f06787917eed758c4222ca12
H1tomaru <watashiwa@inbox.ru>
2018-02-01 23:56:55 +0300

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -27,7 +27,7 @@ after_initialize do
 
 		def show
 			gamelist = @gamedb[:gameDB].find().limit( 10 )
-			userlist = @userlistdb[:ulistP4].find().limit( 10 )
+			userlist = @userlistdb[:uListP4].find().limit( 10 )
 			zapislist = @userdb[:PS4db].find().limit( 10 )
 			feedbacks = @userfb[:userfb].find().limit( 10 )
 			render json: { name: "donut", description: "delicious!", gamelist: gamelist, userlist: userlist, zapislist: zapislist, feedbacks: feedbacks }
