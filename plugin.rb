13f33a297189982d783e655f7c9b0a70bc75f99e
H1tomaru <watashiwa@inbox.ru>
2018-03-11 22:50:38 +0300

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -31,7 +31,7 @@ after_initialize do
 			glist = @@gamedb[:gameDB].find( { _id: { $nin: [ '_encodedcodes' ]} } ).sort( { TYPE: 1, DATE: 1, gameNAME: 1 } ).to_a
 			ulist = @@userlistdb[:uListP4].find().to_a
 			feedbacks = @@userfb[:userfb].find().to_a
-			qzlist = @@gamedb[:gameDB].find( { _id: '_encodedcodes' ).to_a
+			qzlist = @@gamedb[:gameDB].find( _id: '_encodedcodes' ).to_a
 			#if viever registered, count his fb, if guest, just lol
 				
 			glist.each {
