7570ac0658bde2d22a0f2b7fece618e9fbc5710d
H1tomaru <watashiwa@inbox.ru>
2018-03-13 00:03:16 +0300

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -20,25 +20,6 @@ after_initialize do
 
 	class ::MrbugController < ::ApplicationController
 		
-		#encrypt decrypt stuff
-		class String
-		  def encrypt(key)
-		    cipher = OpenSSL::Cipher::Cipher.new('DES-EDE3-CBC').encrypt
-		    cipher.key = Digest::SHA1.hexdigest key
-		    s = cipher.update(self) + cipher.final
-
-		    s.unpack('H*')[0].upcase
-		  end
-
-		  def decrypt(key)
-		    cipher = OpenSSL::Cipher::Cipher.new('DES-EDE3-CBC').decrypt
-		    cipher.key = Digest::SHA1.hexdigest key
-		    s = [self].pack("H*").unpack("C*").pack("c*")
-
-		    cipher.update(s) + cipher.final
-		  end
-		end
-
 		db = Mongo::Client.new([ '93.171.216.230:33775' ], user: 'troiko_user', password: '47TTGLRLR3' )
 		@@gamedb = db.use('AutoZ_gameDB')
 		@@userlistdb = db.use('AutoZ_gameZ')
