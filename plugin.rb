16ee36813070950d802fd1def92c707052c10a53
H1tomaru <watashiwa@inbox.ru>
2018-03-13 01:03:43 +0300

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -25,6 +25,47 @@ after_initialize do
 		@@userlistdb = db.use('AutoZ_gameZ')
 		@@userdb = db.use('userdb')
 		@@userfb = db.use('userfb')
+		
+		def show
+			#db variables
+			ulist = @@userlistdb[:uListP4].find().to_a
+			#other variables
+			finalvar = {}
+			finalvar[:qzstuff] = false
+
+			#if viever registered, count his fb
+			if current_user
+				fbcount = 0
+				feedbacks = @@userfb[:userfb].find( { UID: current_user[:username] } ).to_a
+				feedbacks.each {
+					if feedbacks[:SCORE] < 0
+						fbcount = 0
+						break
+					end
+					fbcount = fbcount + feedbacks[:SCORE]
+				}
+				finalvar[:qzstuff] = true if fbcount >= 10
+			end
+
+			finalvar[:qzstuff] = true
+			#get all games from db and make a qz variable with codes and stuff
+			if finalvar[:qzstuff]
+				glist = @@gamedb[:gameDB].find( { _id: { '$ne': '_encodedcodes' } } ).sort( { gameNAME: 1 } ).to_a
+				qzlist = @@gamedb[:gameDB].find( { _id: '_encodedcodes' } ).to_a
+				dbupdate = {}
+				glist.each {
+					if (qzlist[0][current_user[:username]][glist[:_id]] rescue false)
+						encodedid = qzlist[0][current_user[:username]][glist[:_id]]
+					else
+						encodedid = glist[:_id].encrypt('urban')
+					end
+					finalvar[:qzlist].push( encodedid => glist[:gameNAME] )
+					dbupdate.push( glist[:_id] => encodedid )
+				}
+			end
+
+			render json: { finalvar: finalvar, CurrentUser: current_user, gamelist: glist, userlist: ulist }
+		end
 
 		def troikopoisk
 			#decode shit
