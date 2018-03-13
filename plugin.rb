a4992ab1599d86902fe0078ce57b4964e493c3d4
H1tomaru <watashiwa@inbox.ru>
2018-03-13 04:28:14 +0300

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -74,8 +74,8 @@ after_initialize do
 				dbupdate = {}, finalvar[:qzlist] = {}
 				x = 0
 				glist.each do |game|
-					dbupdate[x] = game
-					dbupdate[x][:id] = game[:gameNAME]
+					dbupdate[x] = game[:_id]
+					#dbupdate[x][:id] = game[:gameNAME]
 					x = x+1
 				#	if (qzlist[0][current_user[:username]][glist[:_id]] rescue false)
 				#		encodedid = qzlist[0][current_user[:username]][glist[:_id]]
