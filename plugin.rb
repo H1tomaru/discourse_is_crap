02f98a1efc070b18d296f04aaed4f6a32de25f6e
H1tomaru <watashiwa@inbox.ru>
2018-03-27 17:45:56 +0300

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -157,6 +157,7 @@ after_initialize do
 						render json: { zaipsfail: true }
 					else
 						#do actual zaips, wohoo
+						push = {}
 						push["P"+code[0]] = { NAME: current_user[:username], DATE: Time.now.strftime("%Y.%m.%d"), STAT: 0}
 						zaips = @@userlistdb[:uListP4].findOneAndUpdate( { _id: code[2] }, { "$push" => push }, { upsert: true } ).to_a
 						render json: { zaips: zaips }
