7fe6601e3946f5fcd3207bd93505d85669d6f435
H1tomaru <watashiwa@inbox.ru>
2018-04-04 00:08:34 +0300

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -83,7 +83,7 @@ after_initialize do
 				#find user for type 0 games and add those type 0 games
 				userDB.each do |user|
 					#see if this user has game data
-					if gameDB.count {|h| h[:_id] == user[:_id] } == 0
+					if gameDB.any? {|h| h[:_id] == ".2dark" } == false #user[:_id]
 						thisgame = @@gamedb[:gameDB].find( { _id: user[:_id] }, projection: { imgLINKHQ: 0 } ).to_a
 						if thisgame[0]
 							gameDB.push(thisgame)
