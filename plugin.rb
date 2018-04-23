92da6aef613f19baa8c7f901eff02e19a779dec0
H1tomaru <watashiwa@inbox.ru>
2018-04-23 18:27:51 +0300

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -603,12 +603,12 @@ after_initialize do
 			uids = db7[:objects].find({ _key: { '$exists': true }, uid: { '$exists': true }, userslug: { '$exists': true } }).to_a
 			feedback3.each do |fb|
 				uid = uids.select {|father| father["uid"] == fb[0][:UID] }
-				if uid[0]
+				if uid
 					thisuserfb = []
 					fb.each do |ufb|
 						thisuserfb.push({ FEEDBACK: ufb }) #{ FEEDBACK: ufb[:FEEDBACK], pNAME: fb[:PNAME], DATE: fb[:DATE], SCORE: fb[:SCORE].to_i, DELETED: false }
 					end
-					finalfb66.push( {_id: uid[0][:username].downcase, FEEDBACKS: thisuserfb } ) #uid[0][:username].downcase
+					finalfb66.push( {_id: uid[:username].downcase, FEEDBACKS: thisuserfb } ) #uid[0][:username].downcase
 				#else
 				#	broken = {uid: fb[0][:UID] }
 				end
