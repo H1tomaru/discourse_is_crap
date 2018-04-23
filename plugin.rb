1a46ce190a6042e3a1e60021bc38d55326157f6f
H1tomaru <watashiwa@inbox.ru>
2018-04-24 01:03:34 +0300

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -604,7 +604,10 @@ after_initialize do
 					thisuserfb = []
 					#userbb = uids.select {|father| father[:uid] == uid[i] }
 					fb.each do |ufb|
-						thisuserfb.push({ FEEDBACK: ufb[:FEEDBACK], pNAME: ufb[:PNAME], DATE: ufb[:DATE], SCORE: ufb[:SCORE].to_i, DELETED: false })
+						thisuserfb.push(ufb) #{ FEEDBACK: ufb[:FEEDBACK], pNAME: ufb[:PNAME], DATE: ufb[:DATE], SCORE: ufb[:SCORE].to_i, DELETED: false }
+					end
+					thisuserfb[1].each do |remake|
+						remake = { FEEDBACK: remake[:FEEDBACK], pNAME: remake[:PNAME], DATE: remake[:DATE], SCORE: remake[:SCORE].to_i, DELETED: false }
 					end
 					lost.push( {_id: uid[i], FEEDBACKS: thisuserfb[1] } )
 					i = i + 1
