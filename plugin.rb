c1629d42b8b2db3ea5b278adad7a908408e782ec
H1tomaru <watashiwa@inbox.ru>
2018-04-24 00:13:34 +0300

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -599,17 +599,17 @@ after_initialize do
 			feedback3 = feedback3.group_by{|h| h[:UID]}
 			db6 = Mongo::Client.new([ '104.244.76.126:33775' ], user: 'h1tomaru', password: 'BZDD7D8BUZ' )
 			db7 = db6.use('nodebb_union')
-			lost = []
+			lost = {}
 			uids = db7[:objects].find({ _key: { '$exists': true }, uid: { '$exists': true }, userslug: { '$exists': true } }).to_a
 			uid = feedback3.keys
 			i = 0
 			feedback3.each do |feedbacks|
-					thisuserfb = []
+					thisuserfb = {}
 					userbb = uids.select {|father| father[:uid] == uid[i] }
 					feedbacks.each do |ufb|
 						thisuserfb.push(ufb) #{ FEEDBACK: ufb[:FEEDBACK], pNAME: fb[:PNAME], DATE: fb[:DATE], SCORE: fb[:SCORE].to_i, DELETED: false }
 					end
-					#lost.push( {_id: uid[i], FEEDBACKS: thisuserfb } ) #userbb[0][:username].downcase
+					lost.push( {_id: uid[i], FEEDBACKS: thisuserfb } ) #userbb[0][:username].downcase
 					i = i + 1
 			end
 
