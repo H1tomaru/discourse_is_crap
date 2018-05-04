01c40da1f99bbb5100ade04c8d28c2ad136d262e
H1tomaru <watashiwa@inbox.ru>
2018-05-04 13:44:51 +0300

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -540,12 +540,13 @@ after_initialize do
 		end
 
 		def feedbacks
-			feedbacks = { MENOSHO: true, fbG: 0, fbB: 0, fbN: 0, FEEDBACKS: false }
+			feedbacks = { MENOSHO: true, fbG: 0, fbB: 0, fbN: 0 }
 			#page owners and users with negative feedbacks cant do feedbacks! 
 			if current_user
 				viewerfb = @@userfb[:userfb].find( { _id: current_user[:username].downcase } ).to_a
 				feedbacks[:MENOSHO] = false if (viewerfb[0] && viewerfb[0][:fbB] && viewerfb[0][:fbB] > 0) || current_user[:username].downcase == params[:username].downcase
 			end
+
 			#find feedbacks from my database
 			userfb = @@userfb[:userfb].find( { _id: params[:username].downcase } ).to_a
 
