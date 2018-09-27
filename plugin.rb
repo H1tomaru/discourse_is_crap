e2d4049f94b9960df70d554e04f2650bc479a6dc
H1tomaru <watashiwa@inbox.ru>
2018-09-27 20:39:03 +0300

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -483,9 +483,9 @@ after_initialize do
 						)
 
 						#create notification if sobrano
-						puts "start"
+						console.log("start")
 						if gameuzers[0] && (gameuzers[0][:P2] || code[0] == 2) && (gameuzers[0][:P4] || code[0] == 4)
-							puts "fullusers"
+							console.log("fullusers")
 							if gameuzers[0]["P"+code[0]]
 								troino = gameuzers[0]["P"+code[0]].count + 1
 							else
@@ -493,10 +493,10 @@ after_initialize do
 							end
 							troino = troino / 2.0 if code[0] == 4
 							trindx = troino - 1
-							puts "troino"+troino
+							console.log("troino"+troino)
 
 							if  (code[0] == 1 && gameuzers[0]["P2"][trindx] && gameuzers[0]["P4"][trindx*2+1]) || (code[0] == 2 && gameuzers[0]["P4"][trindx*2+1]) || (code[0] == 4 && troino.to_i == troino && gameuzers[0]["P2"][trindx])
-								puts "fulltroikaexists"
+								console.log("fulltroikaexists")
 								thisgame = @@gamedb[:gameDB].find( { _id: code[2] } ).to_a
 								if thisgame[0][:PRICE] && !(thisgame[0][:TYPE] == 3 && thisgame[0][:DATE].to_datetime - Time.now > 2600000)
 									usernames = ["MrBug" , current_user[:username]]
