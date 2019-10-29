0302214de850f281f2296d6e5a61f7679910b7d2
H1tomaru <watashiwa@inbox.ru>
2019-10-29 19:38:03 +0300

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -34,16 +34,16 @@ after_initialize do
 
 	class ::MrbugController < ::ApplicationController
 
-		db = Mongo::Client.new([ '104.244.76.126:33775' ], user: 'troiko_user', password: '47TTGLRLR3' )
+		db = Mongo::Client.new([ 'union3.ru:33775' ], user: 'troiko_user', password: '47TTGLRLR3' )
 		@@gamedb = db.use('AutoZ_gameDB')
 		@@userlistdb = db.use('AutoZ_gameZ')
 		@@cache = db.use('AutoZ_cache')
 		@@userdb = db.use('userdb')
 		@@userfb = db.use('userfb')
 
-		@@rentadb = Mongo::Client.new([ '104.244.76.126:33775' ], database: 'rentagadb', user: 'rentaga', password: 'A75Z3E9R66' )
+		@@rentadb = Mongo::Client.new([ 'union3.ru:33775' ], database: 'rentagadb', user: 'rentaga', password: 'A75Z3E9R66' )
 
-		@@userdb2 = Mongo::Client.new([ '104.244.76.126:33775' ], database: 'userdb', user: 'megaadd', password: '3HXED926MT' )
+		@@userdb2 = Mongo::Client.new([ 'union3.ru:33775' ], database: 'userdb', user: 'megaadd', password: '3HXED926MT' )
 		@@userfb2 = @@userdb2.use('userfb')
 
 		def show
