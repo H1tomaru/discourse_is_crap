5b03bd7da7ccacea5ccf7da7cf6f84323deceffc
H1tomaru <watashiwa@inbox.ru>
2019-12-14 03:22:33 +0300

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -851,18 +851,18 @@ after_initialize do
 						if params[:VALUE] == "1"
 							@@rentadb[:rentahideo].find_one_and_update( { _id: current_user[:username].downcase }, {
 							"$push" => { TSHOW: params[:TSHOW] },
-							"$set" => { LIST: { gNAME => true } }
+							"$set" => { LIST: { gNAME: true } }
 							}, { upsert: true } )
 							render json: { HiMom: "!!!!" }
 						else
 							@@rentadb[:rentahideo].find_one_and_update( { _id: current_user[:username].downcase }, {
 							"$pull" => { TSHOW: params[:TSHOW] },
-							"$unset" => { LIST: { gNAME => true } }
+							"$unset" => { LIST: { gNAME: true } }
 							}, { upsert: true } )
 							render json: { HiMom: "!!!!" }
 						end
 					elsif params[:VALUE] == "1"
-						@@rentadb[:rentahideo].insert_one( { _id: current_user[:username].downcase, DATE: Time.now, LIST: { gNAME => true }, TSHOW: params[:TSHOW] } )
+						@@rentadb[:rentahideo].insert_one( { _id: current_user[:username].downcase, DATE: Time.now, LIST: { gNAME: true }, TSHOW: params[:TSHOW] } )
 						render json: { HiMom: "!!!!" }
 					end
 
