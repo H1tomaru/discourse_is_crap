a62eb8c3fec39ff746aa533c13e5f4210f0c8946
H1tomaru <watashiwa@inbox.ru>
2019-12-17 01:37:09 +0300

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -843,7 +843,7 @@ after_initialize do
 
 					#decode shit
 					tSHOW = JSON.parse(URI.unescape(Base64.decode64(params[:TSHOW])))
-					gNAME = tSHOW[:GNAME].to_s
+					gNAME = tSHOW[0][:GNAME]
 
 					rentahideo = @@rentadb[:rentahideo].find( { _id: current_user[:username].downcase } ).to_a
 
