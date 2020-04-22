fa085f6cc6c0d028e0392b9ed3055b29d3b6f6bd
H1tomaru <watashiwa@inbox.ru>
2020-04-22 16:28:24 +0300

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -120,7 +120,7 @@ after_initialize do
 						game[:P4PRICE2] = game[:PRICE] - 2 * game[:P4PRICE3] - game[:P4PRICE1]
 						
 						p4UP = [0,0,150]
-						p4UP = [0,200,150] if game[:PRICE] > 5001
+						p4UP = [0,100,200] if game[:PRICE] > 5001
 						p4UP = [50,0,75] if game[:PRICE] < 2601
 						p4UP = [50,0,50] if game[:PRICE] < 1201
 						p4UP = [0,50,50] if game[:PRICE] < 601
