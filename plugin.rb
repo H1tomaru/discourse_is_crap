40b7918f12db74c3ee263db7d12fc1a2bf54af0a
H1tomaru <watashiwa@inbox.ru>
2022-03-11 10:31:21 +0300

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -131,15 +131,15 @@ after_initialize do
 						elsif game[:PRICE] < 601
 							p4UP = [0,50,50]
 						elsif game[:PRICE] < 1001
-							p4UP = [50,50,50] 
-						elsif game[:PRICE] > 2501
-							p4UP = [50,100,150]
-						elsif game[:PRICE] > 4001
-							p4UP = [100,150,200]
-						elsif game[:PRICE] > 7001	
-							p4UP = [200,200,250] 
+							p4UP = [50,50,50]
 						elsif game[:PRICE] > 10001
 							p4UP = [200,300,350]
+						elsif game[:PRICE] > 7001	
+							p4UP = [200,200,250] 
+						elsif game[:PRICE] > 4001
+							p4UP = [100,150,200]
+						elsif game[:PRICE] > 2501
+							p4UP = [50,100,150]
 						end
 
 						game[:P4PRICE1] = game[:P4PRICE1] - p4PDOWN[1] + p4UP[0]
