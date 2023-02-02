0161e719e6204969a8de460a1a72b014fc7abaae
H1tomaru <watashiwa@inbox.ru>
2023-02-02 19:18:14 +0300

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -715,11 +715,15 @@ after_initialize do
 			#if not exist or old, activate pbot
 			if dendb_date.blank? || dendb_date[:DATE] != timeDAY
 				#res = Faraday.post('https://'+SiteSetting.pbot_ip+'/make_dendb', 'winrars' => true)
-				res = Faraday::Connection.new.post('https://'+SiteSetting.pbot_ip+'/make_dendb', 'winrars' => true) { |request| request.options.timeout = 2 }
-				if res.status == '200'
-					fbglist = {} 	
-				else
-					feedbacks[:test_shit1] = res.status
+				begin
+					res = Faraday::Connection.new.post('https://'+SiteSetting.pbot_ip+'/make_dendb', 'winrars' => true) { |request| request.options.timeout = 2 }
+					if res.status == '200'
+						fbglist = {} 	
+					else
+						feedbacks[:test_shit1] = res.status
+					end
+				rescue
+					feedbacks[:test_shit1] = 'ConnectionFailed'
 				end
 			end
 
