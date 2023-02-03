46e5b68411c8a35db72aa705fd2c31e3ef721e11
H1tomaru <watashiwa@inbox.ru>
2023-02-03 17:40:04 +0300

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -719,7 +719,7 @@ after_initialize do
 			#if not exist or old, activate pbot
 			if dendb_date.blank? || dendb_date[:DATE] != timeDAY
 				begin
-					res = Faraday::Connection.new.post('http://'+SiteSetting.pbot_ip+'/make_dendb', 'winrars' => true) { |request| request.options.timeout = 2 }
+					res = Faraday::Connection.new.post('http://'+SiteSetting.pbot_ip+'/make_dendb', 'winrars' => true) { |request| request.options.timeout = 10 }
 					if res.status == 200
 						fbglist = {}
 					else
