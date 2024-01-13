6656e88bd8bec1829e4d9efd0850ef1c498873ed
H1tomaru <watashiwa@inbox.ru>
2024-01-13 15:39:52 +0300

    Delete assets/javascripts/discourse/templates/component directory

--- assets/javascripts/discourse/templates/component/lazyload.js
+++
@@ -1,8 +0,0 @@
-import Ember from 'ember';
-import $ from 'jquery';
-
-export default Ember.Component.extend({
-    didInsertElement() {
-        $.getScript('https://cdnjs.cloudflare.com/ajax/libs/lazysizes/5.1.2/lazysizes.min.js')
-    }
-})
