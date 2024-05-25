fab7e9c9711db141582a9a6c6362ff6da10eba9b
H1tomaru <watashiwa@inbox.ru>
2024-05-25 04:37:21 +0300

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -1015,7 +1015,7 @@ after_initialize do
 
 			#render json: rentaCache
 			respond_to do |format|
-				format.html { render_json_dump rentaCache }
+				format.html { render "default/empty" }
 				format.json { render json: rentaCache }
 			end
 
