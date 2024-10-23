6cc883d11e1db13d32515282f481e3909618241c
H1tomaru <watashiwa@inbox.ru>
2024-10-23 23:01:16 +0300

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -32,6 +32,9 @@ after_initialize do
 		get '/u/:username/kek' => 'mrbug#feedbacks', constraints: { username: RouteFormat.username }
 		post '/u/:username/kek' => 'mrbug#zafeedback', constraints: { username: RouteFormat.username }
 		post '/u/:username/kek/oishiiii' => 'mrbug#zapass', constraints: { username: RouteFormat.username }
+
+		get '/MrBug2', to: redirect('/MrBug')
+		get '/renta-haleguu2', to: redirect('/renta-haleguu')
 	end
 
 	class ::MrbugController < ::ApplicationController
